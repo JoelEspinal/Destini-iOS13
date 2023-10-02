0bcc018746f291f16265db33c2e37e47ffdc04e9
JoelEspinal <joel_espinal@claro.com.do>
2023-10-02 15:27:52 -0400

    Add completed challenge

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story0: String = "You see a fork in the road."
+    let choice1: String = "Take a left."
+    let choice2: String = "Take a right."
+    var storyBrain: StoryBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        storyLabel.text = story0
+        choice1Button.setTitle(choice1, for: UIControl.State.normal)
+        choice2Button.setTitle(choice2, for: UIControl.State.normal)
     }
 
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let seletedChoice: String = sender.titleLabel!.text!
+        let story: Story = storyBrain.nextStory(seletedChoice)
 
+        updateUI(story: story)
+    }
+    
+    func updateUI(story: Story) {
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: UIControl.State.normal)
+        choice2Button.setTitle(story.choice2, for: UIControl.State.normal)
+    }
 }
-
